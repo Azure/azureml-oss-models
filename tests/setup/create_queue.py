08781f7cb98449eb57a61dab43360dfcd47d2a69
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2023-09-15 12:36:04 +0530

    Update create_queue.py

--- tests/setup/create_queue.py
+++ tests/setup/create_queue.py
@@ -42,7 +42,7 @@ parser.add_argument("--test_trigger_next_model", type=str, default="true")
 parser.add_argument("--test_sku_type", type=str, default="cpu")
 # parallel_tests, to specify number of parallel tests to run per workspace. 
 # this will be used to create multiple queues
-parser.add_argument("--parallel_tests", type=int, default=3)
+parser.add_argument("--parallel_tests", type=int, default=4)
 # workflow-template.yml file to use as template for generating workflow files
 parser.add_argument("--workflow_template", type=str, default="../config/workflow-template-huggingface.yml")
 # workspace_list file get workspace metadata
