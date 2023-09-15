3a4de22997d6dbeae52b100e7dc19ff65344fdbe
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2023-09-15 12:30:46 +0530

    Update generic_initial_automation.py

--- tests/src/automation_for_constant_library/generic_initial_automation.py
+++ tests/src/automation_for_constant_library/generic_initial_automation.py
@@ -71,7 +71,7 @@ def set_next_trigger_model(queue):
 # file the index of test_model_name in models list queue dictionary
     model_list = list(queue.models)
     #model_name_without_slash = test_model_name.replace('/', '-')
-    check_mlflow_model = "MLFlow-"+test_model_name
+    check_mlflow_model = test_model_name
     index = model_list.index(check_mlflow_model)
     #index = model_list.index(test_model_name)
     print(f"index of {test_model_name} in queue: {index}")
